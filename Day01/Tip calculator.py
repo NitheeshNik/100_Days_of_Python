45c18911ce199ad8697d9d38b07a41d5d19eeb1d
NitheeshNik <knitheesh626@gmail.com>
2026-01-25 16:39:31 +0530

    use

--- Day01/Tip calculator.py
+++ Day01/Tip calculator.py
@@ -4,4 +4,4 @@ tip = float(input("How much tip would you like to give? 10, 12, or 15? \n"))
 people = int(input("How many people to split the bill?\n"))
 value_1 = bill / people
 tip_1 = (tip / 100 )*  value_1
-print(f"Each person should pay: ${{{value_1+tip_1:.2f}}}")
+print(f"Each person should pay: ${{{value_1+tip_1:.2f}}}" )

19621a0fa7214eebf4cb727515397d034d8b46e4
NitheeshNik <knitheesh626@gmail.com>
2026-02-02 23:23:32 +0530

    new

---
+++ Day04/password_manager.py
@@ -0,0 +1,18 @@
+pwd = input("What is the master password? ")
+
+def view():
+    pass
+view()
+def add():
+    pass
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view,add) press q to quit. ")
+    if mode == "q":
+        break
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode.")
+        continue

685749bd170e040e9bd7445323d1fabb1be98135
NitheeshNik <knitheesh626@gmail.com>
2026-02-03 14:36:41 +0530

    password_generator

---
+++ Day05/password_generator.py
@@ -0,0 +1,22 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+start = random.randint(0, 52)
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+output = letters[0 : nr_letters ]
+
+output_1 = symbols[0 : nr_symbols ]
+
+output_2 = numbers[0 : nr_numbers ]
+
+add = output + output_1 + output_2
+print(add)
+random.shuffle(add)
+print(add)
+covet = "".join(add)
+print(f"Your password is: {covet}")

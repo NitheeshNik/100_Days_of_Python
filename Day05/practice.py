085844c295008d809ccfc67b2ddece29d0ad9175
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-02-25 23:19:08 +0530

    hangman

--- Day05/practice.py
+++ Day05/practice.py
@@ -1,1 +1,29 @@
+import random
+from hand_art import HANGMANPICS
+# from Hang_words import word
+# print(word[1])
 # Hangman
+# print(
+# """ _
+# | |
+# | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __
+# | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \
+# | | | | (_| | | | | (_| | | | | | | (_| | | | |
+# |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+#                     __/ |
+#                    |___/  """)
+
+# user_guess = input("Guess the letter: ")
+#step - 2 = import the art,words
+#step - 3 = print guess the word
+# step -4 = compare
+# step - 5 = if the answer is correct fill the space to the letter
+# step - 6 = answr wrong hangman
+
+# number = 4
+# print(number * '_')
+n = "1234"
+for i in n :
+    if i == 1:
+        print(i)
+    continue

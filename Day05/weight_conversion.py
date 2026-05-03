7c321cfb82eb79949c71e4c247c6e445d9a3d9c7
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-05-03 12:01:05 +0530

    Weight converstion project

--- Day05/weight_conversion.py
+++ Day05/weight_conversion.py
@@ -3,8 +3,8 @@ choice = int(input("Enter your choice (1 or 2): "))
 if choice == 1:
     kg = float(input("Enter your weight on kg: "))
     kg *= 2.205
-    print(kg)
+    print(f"{kg:.2f}")
 else:
     lb = float(input("Enter your weight on lb: "))
     lb *= 0.451
-    print(lb)
+    print(f"{lb:.2f}")
